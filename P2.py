5282ba92fe78d460333211980533afe276cca63b
ucar98 <alonedircevap@hotmail.com>
2018-11-30 19:13:05 +0300

    Update P2.py

--- P2.py
+++ P2.py
@@ -14,11 +14,16 @@ print()
 print(toplam)
 
 """
-Fibonacci dizisindeki her yeni terim, önceki iki terimi ekleyerek oluşturulur.
+#P2: Fibonacci dizisindeki her yeni terim, önceki iki terimi ekleyerek oluşturulur.
 1 ve 2 ile başlayarak, ilk 10 terim şöyle olacaktır:
 
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
 Değerleri dört milyonu geçmeyen Fibonacci dizisindeki terimleri dikkate alarak,
 ikiye tam bölünen terimlerin toplamını bulun.
+
+Çözüm:
+Üç farklı değer yazdık çünkü fibonacci dizisi eski iki öğenin toplanıp
+yeni öğeye aktarılmasıyla gerçekleşiyordu. Bu yüzden bizde 3 farklı öğeye
+ihtiyaç duyduk.
 """
